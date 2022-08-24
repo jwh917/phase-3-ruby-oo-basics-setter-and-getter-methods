0410d2a69f171832e88e8fbcf0ba5ecc97f1f900
Julien Hnery <jwh_917@yahoo.com>
2022-08-24 07:53:10 -0400

    Build Setter and Getter Methods

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
+  attr_reader :name
+  attr_reader :breed
+
+  # setter method
+  def name=(name)
+    @name = name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
 
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,14 @@
 class Person
+  attr_reader :name
+  attr_reader :job
+
+  # setter method
+  def name=(name)
+    @name = name
+  end
+
+  def job=(job)
+    @job = job
+  end
 
 end
